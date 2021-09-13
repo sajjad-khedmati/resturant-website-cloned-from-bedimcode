80b1de6b2aac906130bdf4bb245b119a19d580fe
sajika <email.gmail.com>
2021-09-13 19:08:26 +0430

    Designed Footer / improve header

--- functionality/main.js
+++ functionality/main.js
@@ -1,7 +1,16 @@
 const btnToggle = document.getElementById('toggler');
 const navMenu = document.querySelector('.navbar-item');
 const navMenuItems = document.querySelectorAll('.item');
+const navbar = document.querySelector('.header')
 
+
+window.addEventListener('scroll' , ()=>{
+    if (window.scrollY>=50) {
+        navbar.classList.add('show-nav')
+    }else{
+        navbar.classList.remove('show-nav')
+    }
+})
 
 const toggle = () => {
     navMenu.classList.toggle('show')
